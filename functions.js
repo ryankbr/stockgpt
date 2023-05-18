b1a4d06eb564ab780f13374628fe389e8ecf0504
Casey <91210043+peicasey@users.noreply.github.com>
2023-05-17 22:51:32 -0500

    fixed conversation display

--- functions.js
+++ functions.js
@@ -1,30 +1,54 @@
 var input = "";
 var response = "this is a temporary response";
-var conversation = "";
+var conversationFormatted = "";
 
-var i = 0;
+var ni = "ni"
+var nr = "nr"
+
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function HandleInput() {
   input = document.getElementById('text-input').value;
+
   document.getElementById('text-input').value = "";
-  document.getElementById('conversation').innerHTML = "<br>" + input + "<br>";
-  conversation += input + "\n";
-  TypeResponse();
-  i = 0;
+
+  setTimeout(TypeInput, speed, 0);
+  setTimeout(TypeResponse, (((speed) * input.length) + 500), 0);
+
+  conversationFormatted += input + "<br><br>" + response + "<br><br>";
   console.log(input);
 };
 
+function TypeInput(i) {
+  if (i < input.length) { 
+    document.getElementById("conversation").innerHTML += input.charAt(i);
+    i++;
+    setTimeout(TypeInput, (speed / 2), i);
+  }
+  else {
+    document.getElementById("conversation").innerHTML += "<br><br>";
+  }
+};
 
-function TypeResponse() {
+function addInpAndRespToConvo() {
+  conversationFormatted += input + "<br><br>" + response + "<br><br>";
+};
+
+function TypeResponse(i) {
   if (i < response.length) { 
     document.getElementById("conversation").innerHTML += response.charAt(i);
     i++;
-    setTimeout(TypeResponse, speed);
+    setTimeout(TypeResponse, speed, i);
   }
-  conversation += response + "\n";
-}
+  else {
+    document.getElementById("conversation").innerHTML += "<br><br>";
+  }
+};
+
+
+
+
 
 function HandleResponse() {
   // TODO: add response to conversation block
-}
+};
